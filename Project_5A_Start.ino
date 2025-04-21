b0799e0c36751330728940e566a5f874d142faff
Mahoutin <mahoutin-zinvo@student.kirkwood.edu>
2025-04-21 09:23:53 -0500

    Update Project_5A_Start.ino
    Changing of the traffic light in each direction

--- Project_5A_Start.ino
+++ Project_5A_Start.ino
@@ -1,24 +1,29 @@
+/* Project 5A - Creating the TRaffic Light 
+ by Mahoutin ZiNvo*/
 
-#define westButton 3
-#define eastButton 13
-#define westRed 2
-#define westYellow 1
+/*PIN 0 to 3 define respectivelly Green, yellow, red and button at west of the traffic*/
 #define westGreen 0
-#define eastRed 12
-#define eastYellow 11
+#define westYellow 1
+#define westRed 2
+#define westButton 3
+
+/* PIN 10 to 13define respectivelly Green, yellow, red and button at east of the traffic */
 #define eastGreen 10
+#define eastYellow 11
+#define eastRed 12
+#define eastButton 13
 
-
+/* yellow blinking tine*/
 #define yellowBlinkTime 500
 
-boolean trafficWest = true; // west = true, east = false
-int flowTime = 10000;
+/* west = true, east = false*/
+boolean trafficWest = true;
+int flowTime = 1000;
 int changeDelay = 2000;
 
 
-
-void setup()
-{
+/*Pin button input and light color (red, yellow, green) output at west and east*/
+void setup() {
   pinMode(westButton, INPUT);
   pinMode(eastButton, INPUT);
   pinMode(westRed, OUTPUT);
@@ -28,67 +33,102 @@ void setup()
   pinMode(eastYellow, OUTPUT);
   pinMode(eastGreen, OUTPUT);
 
+  /*westRed of, WestYellow OFF westGreen ON ,EastRed ON, EastYellow OFF eastGreen OFF*/
   digitalWrite(westRed, LOW);
   digitalWrite(westYellow, LOW);
   digitalWrite(westGreen, HIGH);
   digitalWrite(eastRed, HIGH);
   digitalWrite(eastYellow, LOW);
   digitalWrite(eastGreen, LOW);
+  delay(5000);
 }
 
-void loop()
-{
-  if ( digitalRead(westButton) == HIGH)
-  {
-    if ( trafficWest != true)
-    {
+/*West button can be at usedb to Turn the Colrs traffic ligh ON Or OFF*/
+void loop() {
+  if (digitalRead(westButton) == HIGH) {
+    if (trafficWest != true) {
       trafficWest = true;
       delay(flowTime);
-      digitalWrite(eastGreen, LOW);
+      /*digitalWrite(eastGreen, LOW);
 
+//changing of the east traffic ligh from Green to Red
       digitalWrite(eastYellow, HIGH);
       delay(changeDelay);
       digitalWrite(eastYellow, LOW);
       digitalWrite(eastRed, HIGH);
-      delay(changeDelay);
-      for ( int a = 0; a < 5; a++)
-      {
+      delay(changeDelay);*/
+      changeGreenToRed(eastGreen, eastYellow, eastRed);
+
+      //changing of the west yellow traffic ligh
+      /*for (int a = 0; a < 5; a++) {
         digitalWrite(westYellow, LOW);
         delay(yellowBlinkTime);
         digitalWrite(westYellow, HIGH);
         delay(yellowBlinkTime);
-      }
-      digitalWrite(westYellow, LOW);
-      digitalWrite(westRed, LOW);
-      digitalWrite(westGreen, HIGH);
+      }*/
+      changeYellowLight(westYellow);
 
+      //changing of the west traffic ligh from Red to Green
+      /* digitalWrite(westYellow, LOW);
+      digitalWrite(westRed, LOW);
+      digitalWrite(westGreen, HIGH);*/
+      changeRedToGreen(westGreen, westYellow, westRed);
     }
   }
-  if ( digitalRead(eastButton) == HIGH)
-  {
-    if ( trafficWest == true)
-    {
+  /* East button can be used to turn ON or OFF the traffic ligh */
+  if (digitalRead(eastButton) == HIGH) {
+    if (trafficWest == true) {
       trafficWest = false;
       delay(flowTime);
-      digitalWrite(westGreen, LOW);
+
+      //changing of the west traffic ligh from Green to Red
+      /* digitalWrite(westGreen, LOW);
       digitalWrite(westYellow, HIGH);
       delay(changeDelay);
       digitalWrite(westYellow, LOW);
       digitalWrite(westRed, HIGH);
-      delay(changeDelay);
-      for ( int a = 0; a < 5; a++)
-      {
+      delay(changeDelay);*/
+      changeGreenToRed(westGreen, westYellow, westRed);
+
+      //changing of the east yellow traffic ligh
+      /* for (int a = 0; a < 5; a++) {
         digitalWrite(eastYellow, LOW);
         delay(yellowBlinkTime);
         digitalWrite(eastYellow, HIGH);
         delay(yellowBlinkTime);
-      }
-      digitalWrite(eastYellow, LOW);
+      }*/
+      changeYellowLight(eastYellow);
+
+      //changing of the East traffic ligh from Red to Green
+      /*igitalWrite(eastYellow, LOW);
       digitalWrite(eastRed, LOW);
-      digitalWrite(eastGreen, HIGH);
-
+      digitalWrite(eastGreen, HIGH);*/
+      changeRedToGreen(eastGreen, eastYellow, eastRed);
     }
-
   }
+}
+//changing of the yellow traffic ligh color betweem direction
+void changeYellowLight(int yellowDirection) {
+  for (int a = 0; a < 5; a++) {
+    digitalWrite(yellowDirection, LOW);
+    delay(yellowBlinkTime);
+    digitalWrite(eastYellow, HIGH);
+    delay(yellowBlinkTime);
+  }
+}
+//changing of the traffic ligh between Color trafic ligh
+void changeGreenToRed(int green, int yellow, int red) {
+  digitalWrite(green, LOW);
 
+  digitalWrite(yellow, HIGH);
+  delay(changeDelay);
+  digitalWrite(yellow, LOW);
+  digitalWrite(red, HIGH);
+  delay(changeDelay);
+}
+//changing of the  traffic ligh from Red to Green
+void changeRedToGreen(int green, int yellow, int red) {
+  digitalWrite(yellow, LOW);
+  digitalWrite(red, LOW);
+  digitalWrite(green, HIGH);
 }
